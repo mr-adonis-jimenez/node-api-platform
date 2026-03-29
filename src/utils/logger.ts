017fbd5f0ccccd0ba860ac4f4ec9ab78caa937b9
Adonis Jimenez <233446156+mr-adonis-jimenez@users.noreply.github.com>
2026-03-29 08:52:46 +0000

    feat: scaffold src/utils/logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,18 @@
+import winston from "winston";
+import { env } from "../config/env";
+
+export const logger = winston.createLogger({
+  level: env.LOG_LEVEL,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
